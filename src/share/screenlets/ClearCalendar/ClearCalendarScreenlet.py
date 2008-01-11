c3b2e108912f840fffe3fd2492dbdde87ae8ee60
Helder <helder@helder-laptop>
2008-01-11 02:56:07 +0000

    Added Gmail Screenlet

---
+++ src/share/screenlets/ClearCalendar/ClearCalendarScreenlet.py
@@ -0,0 +1,411 @@
+#!/usr/bin/env python
+
+#  CalculatorScreenlet (c) robgig1088, Vasek Potocek 2007
+#
+# INFO:
+# - a simple example for creating a Screenlet. I shamelessly use RYX's CPU meter as a template
+#
+
+import screenlets
+from screenlets.options import StringOption, BoolOption
+import cairo
+import pango
+import gtk
+import gobject
+import datetime
+import locale
+from iCal import ICalReader
+import sys
+
+
+class ClearCalendarScreenlet(screenlets.Screenlet):
+	"""A simple multilingual iCalendar Screenlet with month preview, you can scroll through other months too and view monthly events."""
+	
+	# default meta-info for Screenlets
+	__name__ = 'ClearCalendarScreenlet'
+	__version__ = '0.4'
+	__author__ = 'Helder Fraga aka Whise based on calendar Screenlet by robgig1088'
+	__desc__ = 'A simple multilingual iCalendar Screenlet with month preview, you can scroll through other months too and view monthly events.'
+
+	# internals
+	__timeout = None
+	__first_day = 0
+	__day_names = []
+
+
+	__buttons_pixmap = None
+	__buttons_alpha = 0
+	__buttons_timeout = None
+	__button_pressed = 0
+	__month_shift = 0
+
+	# settings
+	update_interval = 10
+	first_weekday = ''
+	enable_buttons = True
+	reader = ICalReader()
+	event1 = ''
+
+
+	showevents = True
+	today=datetime.datetime.now().strftime("%F")
+	mypath = sys.argv[0][:sys.argv[0].find('ClearCalendarScreenlet.py')].strip()
+	icalpath = mypath + 'calendar.ics'
+	# constructor
+	def __init__(self, **keyword_args):
+		screenlets.Screenlet.__init__(self, width=int(102*2), height=int(105*2),uses_theme=True, **keyword_args) 
+		# get localized day names
+		locale.setlocale(locale.LC_ALL, '');
+		# we convert to unicode here for the first letter extraction to work well
+		self.__day_names = [locale.nl_langinfo(locale.DAY_1 + i).decode() for i in range(7)] 
+		self.first_weekday = self.__day_names[self.__first_day]
+		# call super (and not show window yet)
+		# set theme
+		self.add_menuitem("icspath", "Ics file path")	
+		self.add_menuitem("events", "View events")
+		self.add_menuitem("mini", "Toggle view events")
+		self.add_menuitem("update", "Update events")	
+		self.theme_name = "default"
+		# add add default menu items
+		self.add_default_menuitems()
+		# add settings
+		self.add_options_group('iCalendar', 'Calendar specific options')
+		self.add_option(StringOption('iCalendar', 'first_weekday', self.first_weekday,
+			'First Weekday', 'The day to be shown in the leftmost column',
+			choices = self.__day_names))
+		self.add_option(BoolOption('iCalendar', 'enable_buttons', self.enable_buttons, 
+			'Enable month shifting', 'Enable buttons selecting another months'))
+		self.add_option(StringOption('iCalendar', 'icalpath', self.icalpath, 'iCalendar ics file path', 'The full path where the .ics file is located , local or url) ...'), realtime=False)
+		self.add_option(BoolOption('iCalendar', 'showevents',bool(self.showevents), 'Show iCalendar events','Show iCalendar events'),realtime=False)
+		# init the timeout functions
+		self.update_interval = self.update_interval
+		self.enable_buttons = self.enable_buttons
+		self.reader.readURL(self.icalpath)
+		self.showevents = self.showevents
+	# attribute-"setter", handles setting of attributes
+	def __setattr__(self, name, value):
+		# call Screenlet.__setattr__ in baseclass (ESSENTIAL!!!!)
+		screenlets.Screenlet.__setattr__(self, name, value)
+		# check for this Screenlet's attributes, we are interested in:
+		if name == ('icalpath'):
+			self.reader = ICalReader()
+			self.reader.readURL(self.icalpath)
+			if self.window:
+				self.redraw_canvas()
+		if name == "update_interval":
+			if value > 0:
+				self.__dict__['update_interval'] = value
+				if self.__timeout:
+					gobject.source_remove(self.__timeout)
+				self.__timeout = gobject.timeout_add(value 
+						* 1000, self.update)
+			else:
+				# TODO: raise exception!!!
+				pass
+		elif name == 'first_weekday':
+			self.__first_day\
+				= self.__day_names.index(self.first_weekday)
+			self.update()
+		elif name == 'enable_buttons':
+			if value == True and not self.__buttons_timeout:
+				self.__buttons_timeout = gobject.timeout_add(100, 
+						self.update_buttons)
+			elif value == False and self.__buttons_timeout:
+				gobject.source_remove(self.__buttons_timeout)
+				self.__buttons_timeout = None
+				self.__buttons_alpha = 0
+				self.update()
+	
+	def get_date_info(self):
+		today = datetime.datetime.now()
+		day = today.day
+		month = today.month
+		year = today.year
+		# apply month shift
+		if self.__month_shift:
+			month += self.__month_shift
+			if month > 12:
+				year += int((month - 1) / 12)
+				month -= (year - today.year) * 12
+			elif month <= 0:
+				year -= int((12 - month) / 12)
+				month += (today.year - year) * 12
+		# get first day of the updated month
+		month_num = datetime.datetime.now().strftime("%m")
+		first_day = datetime.date(year, month, 1)
+		# get the month name
+		month_name = first_day.strftime("%B")
+		month_num = first_day.strftime("%m")
+		# get the day count
+		when = datetime.date(int(year), int(month), int(1))
+		# get the first day of the month (mon, tues, etc..)
+		first_day = when.strftime("%A")
+		# find number of days in the month
+		if month in (1, 3, 5, 7, 8, 10, 12):
+			days_in_month = 31
+		elif month <> 2:
+			days_in_month = 30
+		elif year%4 == 0:
+			days_in_month = 29
+		else:
+			days_in_month = 28
+		#find the first day of the month
+		start_day = int(when.strftime("%u"))  
+		if start_day == 7:				# and do calculations on it...
+			start_day = 0   
+		start_day = start_day + 1
+	
+		# return as array
+		return [day, year, month_name, days_in_month, start_day,month_num]
+	
+	# timeout-functions
+	def update(self):
+		self.icalpath = self.icalpath
+		self.redraw_canvas()
+		return True
+
+	def update_buttons(self):
+		x, y = self.window.get_pointer()
+		x /= (2*self.scale)
+		y /= (2*self.scale)
+		al_last = self.__buttons_alpha
+		if x >= 0 and x < 100 and y >= 0 and y <= 15:	# top line
+			self.__buttons_alpha = min(self.__buttons_alpha + 0.2, 1.0)
+		else:
+			self.__buttons_alpha = max(self.__buttons_alpha - 0.2, 0.0)
+		if self.__buttons_alpha != al_last:
+			self.redraw_canvas()
+		return True
+
+	def on_load_theme(self):
+		self.init_buttons()
+
+	def on_scale(self):
+
+		if self.window:
+			self.init_buttons()
+
+	# redraw button buffer. FIXME: the pixmap is used to enable alpha-rendering of the SVGs.
+	def init_buttons(self):
+		if self.__buttons_pixmap:
+			del self.__buttons_pixmap
+		self.__buttons_pixmap = gtk.gdk.Pixmap(self.window.window, int(self.width 
+			* (2*self.scale)), int(self.height * (2*self.scale)), -1)
+		ctx = self.__buttons_pixmap.cairo_create()
+		self.clear_cairo_context(ctx)
+		ctx.scale(2 *self.scale, 2* self.scale)
+		ctx.set_operator(cairo.OPERATOR_OVER)
+		self.theme['buttons-dim.svg'].render_cairo(ctx)
+		ctx.translate(0, 50)	# bottom half
+		self.theme['buttons-press.svg'].render_cairo(ctx)
+		del ctx
+
+	def on_mouse_down(self, event):
+		if self.enable_buttons and event.button == 1:
+			if event.type == gtk.gdk.BUTTON_PRESS:
+				return self.detect_button(event.x, event.y)
+			else:
+				return True
+		return False
+
+	def on_mouse_up(self, event):
+		# do the active button's action
+		if self.__button_pressed:
+			if self.__button_pressed == 1:
+				self.__month_shift -= 1
+			elif self.__button_pressed == 2:
+				self.__month_shift = 0
+			elif self.__button_pressed == 3:
+				self.__month_shift += 1
+			self.__button_pressed = 0
+			self.redraw_canvas()
+		return False
+
+	def detect_button(self, x, y):
+		x /= (2*self.scale)
+		y /= (2*self.scale)
+
+		button_det = 0
+		if y >= 5.5 and y <= 12.5:
+			if x >= 8.5 and x <= 15.5:
+				button_det = 1
+			elif x >= 18.5 and x <= 25.5:
+				button_det = 2
+			elif x >= 28.5 and x <= 35.5:
+				button_det = 3
+		self.__button_pressed = button_det
+		if button_det:
+			self.redraw_canvas()
+			return True	# we must return boolean for Screenlet.button_press
+		else:
+			return False
+
+	def menuitem_callback(self, widget, id):
+		screenlets.Screenlet.menuitem_callback(self, widget, id)
+
+		if id == "events":
+			screenlets.show_message(self,self.event1)
+			self.redraw_canvas()
+		if id=="icspath":
+			self.show_edit_dialog()
+			
+
+
+		if id == "mini":
+			self.showevents = not self.showevents
+			self.redraw_canvas()
+	
+		if id=="update":
+			
+			self.update()
+	
+	def on_draw(self, ctx):
+		# get data
+		date = self.get_date_info() # [day, year, month_name, days_in_month, start_day]
+		# set size
+		ctx.scale(2*self.scale, 2*self.scale)
+		# draw bg (if theme available)
+		ctx.set_operator(cairo.OPERATOR_OVER)
+		if self.theme:
+			self.theme['date-bg.svg'].render_cairo(ctx)
+			#self.theme['date-border.svg'].render_cairo(ctx)
+		# draw buttons and optionally the pressed one
+		if self.__buttons_pixmap:
+			ctx.save()
+			ctx.rectangle(0, 0, 100, 15)
+			ctx.clip()
+			ctx.identity_matrix()
+			ctx.set_source_pixmap(self.__buttons_pixmap, 0, 0)
+			ctx.paint_with_alpha(self.__buttons_alpha)
+			ctx.restore()
+			if self.__button_pressed:
+				ctx.save()
+				ctx.rectangle(26.5 + self.__button_pressed * 10, 0, 10, 15)
+				ctx.clip()
+				ctx.identity_matrix()
+				# use bottom half of the pixmap
+				ctx.set_source_pixmap(self.__buttons_pixmap, 0, -50 * 2* self.scale)
+				ctx.paint()
+				ctx.restore()
+				
+		# draw the calendar foreground
+		if self.theme:
+			ctx.save()
+			ctx.translate(5,5)
+			p_layout = ctx.create_layout()
+			p_fdesc = pango.FontDescription()
+			p_fdesc.set_family_static("Tahoma")
+			p_fdesc.set_size(5 * pango.SCALE)
+			p_layout.set_font_description(p_fdesc)      ### draw the month
+			p_layout.set_width((self.width - 10) * pango.SCALE)
+			p_layout.set_markup('<b>' + date[2] + '</b>')
+			ctx.set_source_rgba(1, 1, 1, 0.8)
+			
+			
+			#ctx.show_layout(p_layout)
+		
+			ctx.translate(-100,0)
+			p_layout.set_width((self.width - 10) * pango.SCALE)
+			p_layout.set_alignment(pango.ALIGN_RIGHT)
+			p_layout.set_markup("<b>" + date[2]+' '+ str(date[1])+ '  </b>') ### draw the year
+			ctx.set_source_rgba(1, 1, 1, 0.8)
+			ctx.show_layout(p_layout)
+
+			ctx.restore()
+			ctx.save()
+
+			ctx.translate(0, 15)
+			#self.theme['header-bg.svg'].render_cairo(ctx)  #draw the header background
+			ctx.translate(6, 0)
+			p_fdesc.set_size(4 * pango.SCALE)
+			p_layout.set_font_description(p_fdesc) 
+			#Draw header
+			p_layout.set_alignment(pango.ALIGN_CENTER);
+			p_layout.set_width(10*pango.SCALE);
+			self.event1 = ''
+			for i in range(7):
+				dayname = self.__day_names[(i \
+					+ self.__first_day) % 7]
+				p_layout.set_markup("<b><span font_desc='Monospace'>" + dayname[:3] + '</span></b>') # use first letter
+				ctx.set_source_rgba(1, 1, 1, 0.8)
+				ctx.show_layout(p_layout)
+				ctx.translate(13, 0)	# 6 + 6*13 + 6 = 100
+			p_fdesc.set_size(6 * pango.SCALE)
+			p_fdesc.set_family_static("Free Sans")
+
+			p_layout.set_font_description(p_fdesc) 
+			# Draw the day labels
+			ctx.restore()
+			row = 1
+
+			day = (int(date[4]) + 7 - self.__first_day)%7
+			if day == 0 :
+				day = 7
+			for x in range(date[3]):
+				ctx.save()
+
+				if row == 6:
+					row = 1
+				ctx.translate(6 + (day - 1)*13, 25 + 12*(row - 1))
+				#print str(6 + (day - 1)*13)
+				#print str( 25 + 12*(row - 1))
+				if self.__month_shift == 0 and int(x)+1 == int(date[0]):
+					self.theme['day-bg1.svg'].render_cairo(ctx)
+				if self.showevents == True:
+					for event in self.reader.events:
+						
+						myevent = str(event.startDate)
+						myevent  = myevent[:myevent.find(' ')].strip()
+	
+						a = str(x +1)
+						if len(a) == 1:
+							a = '0' + a
+					
+						if myevent == str(date[1]) + '-' + str(date[5])+ '-' + str(a) :
+							self.theme['day-bg.svg'].render_cairo(ctx)
+							if int(date[1]) >= int(self.today[:4]) or int(date[1]) >= int(self.today[:4]) and int(date[5]) >= int(self.today[5:7]) :
+								
+								self.event1 = self.event1 + '\n'+ str(date[1]) + '-' + str(date[5])+ '-' + str(a)+ ' - ' +str(event)
+						if myevent == datetime.datetime.now().strftime("%F") and self.__month_shift == 0 and int(x)+1 == int(date[0]) :
+							self.theme['day-bg2.svg'].render_cairo(ctx)
+							self.event1 = self.event1 + '\n Today - '+ str(event)
+				p_layout.set_markup( str(x+1) )
+	
+				ctx.set_source_rgba(1, 1, 1, 1)
+				ctx.show_layout(p_layout)
+				if day == 7:
+					day = 0
+					row = row + 1
+				day = day + 1
+				ctx.restore()
+
+	def show_edit_dialog(self):
+		# create dialog
+		dialog = gtk.Dialog("iCalendar ics path", self.window)
+		dialog.resize(300, 100)
+		dialog.add_buttons(gtk.STOCK_OK, gtk.RESPONSE_OK, 
+			gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL)
+		entrybox = gtk.Entry()
+		entrybox.set_text(str(self.icalpath))
+		dialog.vbox.add(entrybox)
+		entrybox.show()	
+		# run dialog
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			self.icalpath = entrybox.get_text()
+			self.updated_recently = 1
+		dialog.hide()
+		self.update()
+
+	def on_draw_shape(self,ctx):
+		if self.theme:
+			# set size rel to width/height
+			ctx.scale(2*self.scale, 2*self.scale)
+			self.theme['date-bg.svg'].render_cairo(ctx)
+	
+
+# If the program is run directly or passed as an argument to the python
+# interpreter then create a Screenlet instance and show it
+if __name__ == "__main__":
+	import screenlets.session
+	screenlets.session.create_session(ClearCalendarScreenlet)
---
+++ src/share/screenlets/ClearCalendar/iCal.py
@@ -0,0 +1,370 @@
+#!/usr/bin/python
+############################################################################
+# Programmers: Jiva DeVoe <jiva@devoesquared.com>
+# Filename: iCal.py
+# http://www.devoesquared.com/Software/iCal_Module
+#
+# Contributors / Alterations:
+# stefan natchev: april 15, 2005 - added support for dates with ranges
+# stefan natchev: april 16, 2005 - added more recurrance support
+# Aaron A. Seigo aseigo@kde.org: January 2006 - patches for event lookup for multi-day events. (see occursOn)
+# Danil Dotsenko dd@accentsolution.com: June 15, 2006 - reworked file rutine in ICalReader to work with generic paths and added fileFilter and check for correct header.
+# Danil Dotsenko dd@accentsolution.com: -----||------ - reworked other functions for efficiency.
+# Paul van Erk: July 3, 2006 - fixed a bug where all-day events would be counted as 2 days (and 2-day events as 3, etc)
+# Danil Dotsenko dd@accentsolution.com: Sept 04, 2006 - changed the iCalReader class init rutine to init on the basis of raw data list, not filenames. This allows reading files from remote sources.
+#  Warning! with this version (20060904) I am braking backward compatibility in __init__() arguments of ICalReader
+#
+# version 20060904
+#
+# "2-Clause" BSD license
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+############################################################################
+
+import os
+import os.path
+import re
+import datetime
+import time
+
+class ICalReader:
+	def __init__(self, dataLines = None):
+		'''
+		iCal.ICalReader([dataList])
+
+		Initializes the iCal reader and parses the list type object for events.
+		The list type object with iCal contents is optional.
+		You can call readFiles(file name list) and readEvents(iCal data lines) separately.
+		'''
+		self.events = []
+		if dataLines:
+			self.readEvents(dataLines)
+
+	def readFiles(self, fileList):
+		'''
+		readFiles(fileList)
+
+		This function accepts ARRAY with list of local file names.
+		We put contents of the files into one big list and feed to readEvents()
+		We only work with files, no folders or wildcards.
+		'''
+		dataLines = []
+		for item in range(fileList.__len__()):
+			if os.path.isfile(fileList[item]): # not sure why I am doing it, your code should...
+				tempFile = open(fileList[item], 'r')
+				dataLines.extend(tempFile.readlines())
+				tempFile.close()
+		if dataLines:
+			self.readEvents(dataLines)
+
+	def readURL(self, url):
+		'''
+		Put rutine to fetch and convert a resource into lines and feed it to readEvents()
+		'''
+		dataLines = None
+		try:
+			import urllib2
+			tempFile = urllib2.urlopen(url)
+			dataLines = tempFile.readlines()
+			tempFile.close()
+		except:
+			tempFile = open(url,'r')
+			dataLines = tempFile.readlines()
+			tempFile.close()
+		if dataLines:
+			self.readEvents(dataLines)
+
+	def readEvents(self, lines, emptyEvents=False):
+		if emptyEvents:
+			self.events = []
+			# this is here for MY convenience. Don't rely on this.
+			# Instead just to iCalObject.events = {} in your code.
+			# The latter allows emptying events with functions like readURL and readFiles
+		mask = {}
+		mask['BEGIN'] = re.compile("^BEGIN:VEVENT")
+		mask['END'] = re.compile("^END:VEVENT")
+		inEvent= False
+		for line in lines: # this will only work if there is no such thing as nested VEVENTs. It assumes there is one END:VEVENT fro every openning BEGIN
+			if mask['BEGIN'].match(line):
+				eventLines = []
+				inEvent = True
+			elif mask['END'].match(line) and inEvent:
+				# note: BEGIN: and END: are not included in the list.
+				self.events.append(self.parseEvent(eventLines))
+				inEvent = False
+			elif inEvent:
+				eventLines.append(line)
+
+	def parseEvent(self, lines):
+		event = ICalEvent()
+		startDate = None
+		rule = None
+		endDate = None
+		#it has to have something for a summary(?) -s
+		event.summary = ''
+		mask={}
+		mask['Summary']=re.compile("^SUMMARY:(.*)")
+		mask['DTStart']=re.compile("^DTSTART;.*:(.*).*")
+		mask['DTEnd']=re.compile("^DTEND;.*:(.*).*")
+		mask['DTStartTZ']=re.compile("^DTSTART:(.*)T.*Z")
+		mask['DTEndTZ']=re.compile("^DTEND:(.*)T.*Z")
+		mask['ExDate']=re.compile("^EXDATE.*:(.*)")
+		mask['RRule']=re.compile("^RRULE:(.*)")
+		timed = '1'
+		for line in lines:
+			if mask['Summary'].match(line):
+				event.summary = mask['Summary'].match(line).group(1)
+			#these are the dtstart/dtend with no time range
+			elif mask['DTStart'].match(line):
+				startDate = self.parseDate(mask['DTStart'].match(line).group(1))
+				timed = '0'
+			elif mask['DTEnd'].match(line):
+				endDate = self.parseDate(mask['DTEnd'].match(line).group(1))
+				timed = '0'
+			#these are the ones that are 'ranged'
+			elif mask['DTStartTZ'].match(line):
+				startDate = self.parseDate(mask['DTStartTZ'].match(line).group(1))
+			elif mask['DTEndTZ'].match(line):
+				endDate = self.parseDate(mask['DTEndTZ'].match(line).group(1))
+			elif mask['ExDate'].match(line):
+				event.addExceptionDate(self.parseDate(mask['ExDate'].match(line).group(1)))
+			elif mask['RRule'].match(line):
+				rule = mask['RRule'].match(line).group(1)
+		event.startDate = startDate
+		event.endDate = endDate
+		event.timed = timed
+		if rule:
+			event.addRecurrenceRule(rule)
+		return event
+
+	#def parseTodo(self, lines):
+	#	todo = ICalTodo()
+	#	startDate = None
+	#	endDate = None
+
+	def parseDate(self, dateStr):
+		year = int(dateStr[0:4])
+		if year < 1970:
+			year = 1970
+		month = int(dateStr[4:4+2])
+		day = int(dateStr[6:6+2])
+		try:
+			hour = int(dateStr[9:9+2])
+			minute = int(dateStr[11:11+2])
+		except:
+			hour = 0
+			minute = 0
+		return datetime.datetime(year, month, day, hour, minute)
+
+	def selectEvents(self, selectFunction):
+		note = datetime.datetime.today()
+		self.events.sort()
+		events = filter(selectFunction, self.events)
+		return events
+
+	def todaysEvents(self, event):
+		return event.startsToday()
+
+	def tomorrowsEvents(self, event):
+		return event.startsTomorrow()
+
+	def afterTodaysEvents(self, event):
+		return event.startsAfterToday()
+
+	def eventsFor(self, date):
+		note = datetime.datetime.today()
+		self.events.sort()
+		ret = []
+		for event in self.events:
+			#if event.startsOn(date):
+			if event.occursOn(date):
+				ret.append(event)
+		return ret
+
+		year = int(dateStr[0:4])
+		if year < 1970:
+			year = 1970
+
+		month = int(dateStr[4:4+2])
+		day = int(dateStr[6:6+2])
+		try:
+			hour = int(dateStr[9:9+2])
+			minute = int(dateStr[11:11+2])
+		except:
+			hour = 0
+			minute = 0
+
+class ICalEvent:
+	def __init__(self):
+		self.exceptionDates = []
+		self.dateSet = None
+
+	def __str__(self):
+		return self.summary
+
+	def __eq__(self, otherEvent):
+		return self.startDate == otherEvent.startDate
+
+	def addExceptionDate(self, date):
+		self.exceptionDates.append(date)
+
+	def addRecurrenceRule(self, rule):
+		self.dateSet = DateSet(self.startDate, self.endDate, rule)
+
+	def startsToday(self):
+		return self.startsOn(datetime.datetime.today())
+
+	def startsTomorrow(self):
+		tomorrow = datetime.datetime.fromtimestamp(time.time() + 86400)
+		return self.startsOn(tomorrow)
+
+	def startsAfterToday(self):
+		return self.startsAfter(datetime.datetime.today())
+
+	def startsOn(self, date):
+		return (self.startDate.year == date.year and
+			self.startDate.month == date.month and
+			self.startDate.day == date.day or
+			(self.dateSet and self.dateSet.includes(date)))
+
+	def occursOn(self, date):
+		if (self.timed == '0'):
+			return (self.startsOn(date) or (self.startDate < date and self.endDate >= date) and self.endDate != date)
+		else:
+			return (self.startsOn(date) or (self.startDate < date and self.endDate >= date))
+
+	def startsAfter(self, date):
+		return (self.startDate > date)
+
+	def startTime(self):
+		return self.startDate
+
+#class ICalTodo:
+
+#strange...
+#class DateParser:
+def parse(dateStr):
+	year = int(dateStr[0:4])
+	if year < 1970:
+		year = 1970
+
+	month = int(dateStr[4:4+2])
+	day = int(dateStr[6:6+2])
+	try:
+		hour = int(dateStr[9:9+2])
+		minute = int(dateStr[11:11+2])
+	except:
+		hour = 0
+		minute = 0
+	return datetime.datetime(year, month, day, hour, minute)
+
+
+class DateSet:
+	def __init__(self, startDate, endDate, rule):
+		self.startDate = startDate
+		self.endDate = endDate
+		self.startWeekNumber = startDate.isocalendar()[1]
+		self.frequency = None
+		self.count = None
+		self.interval = 1
+		self.untilDate = None
+		self.byMonth = None
+		self.byDate = None
+		self.parseRecurrenceRule(rule)
+
+	def parseRecurrenceRule(self, rule):
+		if re.compile("FREQ=(.*?);").match(rule):
+			self.frequency = re.compile("FREQ=(.*?);").match(rule).group(1)
+
+		if re.compile("COUNT=(\d*)").search(rule):
+			self.count = int(re.compile("COUNT=(\d*)").search(rule).group(1))
+
+		if re.compile("UNTIL=(.*?);").search(rule):
+			#homebrewed
+			self.untilDate = parse(re.compile("UNTIL=(.*?);").search(rule).group(1))
+		if re.compile("INTERVAL=(\d*);").search(rule):
+			self.interval = int(re.compile("INTERVAL=(\d*);").search(rule).group(1))
+
+		if re.compile("BYMONTH=(.*?);").search(rule):
+			self.byMonth = re.compile("BYMONTH=(.*?);").search(rule).group(1)
+
+		if re.compile("BYDAY=(.*?);").search(rule):
+			self.byDay = re.compile("BYDAY=(.*?);").search(rule).group(1)
+
+
+
+	def includes(self, date):
+		if date == self.startDate:
+			return True
+
+		if self.untilDate and date > self.untilDate:
+			return False
+
+		if self.frequency == 'DAILY':
+			if self.interval:
+				increment = self.interval
+			else:
+				increment = 1
+			d = self.startDate
+			counter = 0
+			while(d < date):
+				if self.count:
+					counter += 1
+					if counter >= self.count:
+						return False
+
+				d = d.replace(day=d.day+1)
+
+				if (d.day == date.day and
+					d.year == date.year and
+					d.month == date.month):
+					return True
+
+		elif self.frequency == 'WEEKLY':
+			if self.startDate.weekday() == date.weekday():
+			   #make sure the interval is proper -Milo
+			   if self.startWeekNumber % self.interval == date.isocalendar()[1] % self.interval:
+				  return True
+			   else:
+				  return False
+			else:
+				if self.endDate:
+					for n in range(0, self.endDate.day - self.startDate.day):
+						newDate = self.startDate.replace(day=self.startDate.day+n)
+						if newDate.weekday() == date.weekday():
+							return True
+
+		elif self.frequency == 'MONTHLY':
+			pass
+
+		elif self.frequency == 'YEARLY':
+			pass
+
+		return False
+
+
+if __name__ == '__main__':
+	reader = ICalReader()
+	# reader.readURL('http://www.google.com/calendar/ical/4dmslp71qlvjhl1q6g6f88gfv4@group.calendar.google.com/public/basic.ics')
+	reader.readURL('file:///home/dd/musor/icalout.ics')
+	for event in reader.events:
+		print event

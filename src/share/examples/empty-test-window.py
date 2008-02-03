89ad7f4b8eb51b3f4f8b593b310d55d38f82cb60
Helder <helder@helder-laptop>
2008-02-03 19:04:28 +0000

    This revision is marked as stable ready for release

--- src/share/examples/empty-test-window.py
+++ src/share/examples/empty-test-window.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python
+
+# This application is released under the GNU General Public License 
+# v3 (or, at your option, any later version). You can find the full 
+# text of the license under http://www.gnu.org/licenses/gpl.txt. 
+# By using, editing and/or distributing this software you agree to 
+# the terms and conditions of this license. 
+# Thank you for using free software!
+
+# (c) 2007 RYX (aka Rico Pfaus) <ryx@ryxperience.com>
+
 import gtk
+
+
 #from gobject import GObject
 
 # A simple testbed

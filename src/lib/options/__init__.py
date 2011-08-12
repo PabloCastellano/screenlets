530425a591bab6f4cdc6b96ce5ab4be7394333c2
Guido Tabbernuk <boamaod@gmail.com>
2011-08-12 22:04:26 +0300

    Update licenses for options files.

--- src/lib/options/__init__.py
+++ src/lib/options/__init__.py
@@ -1,12 +1,20 @@
-# This application is released under the GNU General Public License
-# v3 (or, at your option, any later version). You can find the full
-# text of the license under http://www.gnu.org/licenses/gpl.txt.
-# By using, editing and/or distributing this software you agree to
-# the terms and conditions of this license.
-# Thank you for using free software!
-
+#
 # Options-system (c) RYX (aka Rico Pfaus) 2007 <ryx@ryxperience.com>
 # Heavily Refactored by Martin Owens (c) 2009
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #
 # INFO:
 # - a dynamic Options-system that allows very easy creation of

90d083a3594131b553a89f0f34341aeecfddf367
Guido Tabbernuk <boamaod@gmail.com>
2011-08-12 22:07:41 +0300

    Update old GPL licenses to latest form in most of files.

--- src/lib/sensors.py
+++ src/lib/sensors.py
@@ -1,9 +1,15 @@
-# This application is released under the GNU General Public License 
-# v3 (or, at your option, any later version). You can find the full 
-# text of the license under http://www.gnu.org/licenses/gpl.txt. 
-# By using, editing and/or distributing this software you agree to 
-# the terms and conditions of this license. 
-# Thank you for using free software!
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Sensors Module (c) Whise (Helder Fraga) 2008 <helder.fraga@hotmail.com>
 
